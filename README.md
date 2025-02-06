135c720c09c2c8866d625d11ce21a4a2ddece37e
caschwarz <clarkaschwarz@outlook.com>
2025-02-05 19:14:21 -0500

    finished the report and documentation

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# database_hw1
+This program will take in a name of a .pcap file and will sort out the packets
+from it that do not meet the parameters specified. It will then print
+the different headers for each packet remaining.
+
+To run the program, use: python pktsniffer.py -r [file_name.pcap]
+
+if you wish to add filters to it, you can use these flags as follows - 
+
+-c [number] = the maximum number of packets to look through
+
+-port [number] = the packets must have this port number to be displayed
+
+-ip [string] = the packets must have this provided ip in either their
+destination or send address
+
+-proto [string] = the packets must have used this protocol to be included.
+valid options are either tcp, udp or icmp
+
+
+Examples of command line usage:
+
+python pktsniffer.py -r data.pcap -c 5 -proto tcp
+
+python pktsniffer.py -r data2.pcap -c 200 -port 80
---
+++ conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Packet Analyzer'
+copyright = '2025, Clark Schwarz'
+author = 'Clark Schwarz'
+release = '2/5/2025'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'python'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
